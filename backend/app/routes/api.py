900673ccd5e04969610cbfa75e85da2052749d2c
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-10-30 00:29:27 +0530

    add userId parameter, changed ui of Story page

--- backend/app/routes/api.py
+++ backend/app/routes/api.py
@@ -1,12 +1,16 @@
 from app.services.gemini_api import generate_story
 from app.services.lstm_model import generate_words
 from app.services.mood_detector import detect_mood
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile,Form
 
 router = APIRouter()
 
 @router.post("/generate-story")
-async def generate_story_endpoint(image: UploadFile = File(...), audio: UploadFile = File(...), genre: str = "any"):
+async def generate_story_endpoint(    
+    image: UploadFile = File(...),
+    audio: UploadFile = File(...),
+    userId: str = Form(...),
+    genre: str = Form("any")):
     # Save files
     img_path = f"uploads/{image.filename}"
     audio_path = f"uploads/{audio.filename}"
@@ -25,10 +29,11 @@ async def generate_story_endpoint(image: UploadFile = File(...), audio: UploadFi
     story_lines = story.split("\n", 1)  # split at the first newline
     title = story_lines[0].strip() if len(story_lines) > 0 else ""
     body = story_lines[1].strip() if len(story_lines) > 1 else ""
-
+    
     return {
         "mood": mood,
         "words": words,
         "title": title,
-        "body": body
+        "body": body,
+        "userId":userId
     }
